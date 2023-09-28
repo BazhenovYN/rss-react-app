7a889b4f8fcd8236ec122f1cecdaf529ebb76b06
BazhenovYN <BazhenovYN@gmail.com>
2023-09-28 22:33:07 +0200

    feat: add vitest config

---
+++ src/app/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  test('renders correctly', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Hello world');
+  });
+});
