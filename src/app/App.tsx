d4dca113acab780ffcf25e61e8433a841ada5d2d
Iurii Bazhenov <bazhenovyn@gmail.com>
2023-10-28 15:51:39 +0200

    feat: remove unnecessary functionality

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,14 @@
 import { Component } from 'react';
 import ErrorBoundary from '@/components/features/ErrorBoundary';
-import { NotificationProvider } from '@/components/features/NotificationManager';
 import Home from '@/pages/Home';
 
 export default class App extends Component {
   render() {
     return (
       <ErrorBoundary>
-        <NotificationProvider>
-          <main>
-            <Home />
-          </main>
-        </NotificationProvider>
+        <main>
+          <Home />
+        </main>
       </ErrorBoundary>
     );
   }
