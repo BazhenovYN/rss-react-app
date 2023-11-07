ff5912c28f909a0cf7ff2adc427b0e752cfb60fb
Iurii Bazhenov <bazhenovyn@gmail.com>
2023-11-07 12:09:08 +0100

    feat: set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,17 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text'],
+      all: true,
+      exclude: ['**/index.ts', '**/types.ts', '**/*.d.ts'],
+      watermarks: {
+        lines: [50, 80],
+        functions: [50, 80],
+        branches: [50, 80],
+        statements: [50, 80],
+      },
+    },
   },
 });
