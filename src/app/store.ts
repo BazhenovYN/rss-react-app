5d05e4c14f7ced7eb9e1080c64c2478648f5f560
Iurii Bazhenov <bazhenovyn@gmail.com>
2023-11-17 14:38:35 +0100

    feat: setting up a reusable test render function

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import type { PreloadedState } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { api } from '@/services/star-wars';
 import searchReducer from '@/store/searchSlice';
 
-export const store = configureStore({
-  reducer: {
-    [api.reducerPath]: api.reducer,
-    search: searchReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+const rootReducer = combineReducers({
+  [api.reducerPath]: api.reducer,
+  search: searchReducer,
 });
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
+  });
+};
+
+const store = setupStore();
 
 setupListeners(store.dispatch);
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof setupStore>;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = AppStore['dispatch'];
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from '@/app';
-import { store } from './app/store';
+import { setupStore } from '@/app/store';
 
 import '@/styles/global.scss';
+
+const store = setupStore();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
